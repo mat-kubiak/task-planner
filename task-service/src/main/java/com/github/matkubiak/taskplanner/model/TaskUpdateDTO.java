028637e53e500f30e0bec699b583dcd8dbd34a48
Mateusz Kubiak <matkubiak888@gmail.com>
2024-11-01 21:57:59 +0100

    feat: new DTO for updating tasks

---
+++ task-service/src/main/java/com/github/matkubiak/taskplanner/model/TaskUpdateDTO.java
@@ -0,0 +1,41 @@
+package com.github.matkubiak.taskplanner.model;
+
+public class TaskUpdateDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private boolean state;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean getState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+}
