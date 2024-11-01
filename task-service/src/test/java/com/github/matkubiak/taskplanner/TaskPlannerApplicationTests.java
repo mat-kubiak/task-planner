9dd1194c22862a8a2679c11048af49c79a334c86
Mateusz Kubiak <matkubiak888@gmail.com>
2024-11-02 00:12:42 +0100

    style: include publication year in copyright notice

--- task-service/src/test/java/com/github/matkubiak/taskplanner/TaskPlannerApplicationTests.java
+++ task-service/src/test/java/com/github/matkubiak/taskplanner/TaskPlannerApplicationTests.java
@@ -1,10 +1,10 @@
 /*
-  Task Planner
-  Copyright (C) Mateusz Kubiak
-
-  Licensed under the GNU General Public License v3. 
-  See LICENSE or visit <https://www.gnu.org/licenses/>.
-*/
+ * Task Planner
+ * Copyright (C) 2024 Mateusz Kubiak
+ *
+ * Licensed under the GNU General Public License v3.
+ * See LICENSE or visit <https://www.gnu.org/licenses/>.
+ */
 
 package com.github.matkubiak.taskplanner;
 
