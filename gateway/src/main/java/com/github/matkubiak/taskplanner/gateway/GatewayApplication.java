2d8585520b25a8dde3b37a8bd74b2a82e72403ee
Mateusz Kubiak <matkubiak888@gmail.com>
2024-11-02 16:08:47 +0100

    feat: configured template gateway project to docker

---
+++ gateway/src/main/java/com/github/matkubiak/taskplanner/gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+/*
+ * Task Planner
+ * Copyright (C) 2024 Mateusz Kubiak
+ *
+ * Licensed under the GNU General Public License v3.
+ * See LICENSE or visit <https://www.gnu.org/licenses/>.
+ */
+
+package com.github.matkubiak.taskplanner.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
