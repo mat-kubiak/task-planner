950de0911408638688905292b7b6d86445553b8c
Mateusz Kubiak <matkubiak888@gmail.com>
2024-11-02 01:02:31 +0100

    feat!: created empty user service
    Api changes:
    * new endpoint for getting health status: GET `/api/users/health`

---
+++ user-service/src/test/java/com/github/matkubiak/taskplanner/UserPlannerApplicationTests.java
@@ -0,0 +1,21 @@
+/*
+ * Task Planner
+ * Copyright (C) 2024 Mateusz Kubiak
+ *
+ * Licensed under the GNU General Public License v3.
+ * See LICENSE or visit <https://www.gnu.org/licenses/>.
+ */
+
+package com.github.matkubiak.taskplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
