cf7162a90a7cacc0bc066254d574b34daf0b24d2
Mateusz Kubiak <matkubiak888@gmail.com>
2024-11-02 16:25:02 +0100

    feat: basic routes

--- gateway/src/main/java/com/github/matkubiak/taskplanner/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/github/matkubiak/taskplanner/gateway/GatewayApplication.java
@@ -10,12 +10,30 @@ package com.github.matkubiak.taskplanner.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/api/tasks")
+						.uri("http://task-service:8080/api/tasks/"))
+				.route(p -> p
+						.path("/api/tasks/health")
+						.uri("http://task-service:8080/api/tasks/health"))
+				.route(p -> p
+						.path("/api/users/health")
+						.uri("http://user-service:8080/api/users/health"))
+				.build();
 	}
 
 }
