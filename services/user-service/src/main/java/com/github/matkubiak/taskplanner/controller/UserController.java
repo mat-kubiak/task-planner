0d9009037f46e332e460519f831b01c3349f9980
Mateusz Kubiak <matkubiak888@gmail.com>
2024-11-11 20:09:08 +0100

    feat!: user GET request
    API changes:
    * new (temporary) GET request at `/api/users/` to get all users

--- services/user-service/src/main/java/com/github/matkubiak/taskplanner/controller/UserController.java
+++ services/user-service/src/main/java/com/github/matkubiak/taskplanner/controller/UserController.java
@@ -8,15 +8,29 @@
 
 package com.github.matkubiak.taskplanner.controller;
 
+import com.github.matkubiak.taskplanner.model.User;
+import com.github.matkubiak.taskplanner.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class UserController {
+
+    @Autowired
+    private UserService service;
 
     @GetMapping(path="/health")
     public ResponseEntity<Object> healthCheck() {
         return new ResponseEntity<>("Service is up and running!", HttpStatus.OK);
+    }
+
+    @GetMapping(path="/")
+    public ResponseEntity<Object> getAllUsers() {
+        List<User> users = service.getUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 }
