115ddb52084f08c1c7ace24c6e97bd2cf60859c4
Mateusz Kubiak <matkubiak888@gmail.com>
2024-11-11 20:08:26 +0100

    feat: user entity

---
+++ services/user-service/src/main/java/com/github/matkubiak/taskplanner/model/User.java
@@ -0,0 +1,55 @@
+/*
+ * Task Planner
+ * Copyright (C) Mateusz Kubiak
+ *
+ * Licensed under the GNU General Public License v3.
+ * See LICENSE or visit <https://www.gnu.org/licenses/>.
+ */
+
+package com.github.matkubiak.taskplanner.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long userId;
+
+    @NotNull
+    @Length(min=1, max=250)
+    private String email;
+
+    @NotNull
+    @Length(min=8, max=50)
+    private String password;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public @Length(min = 1, max = 250) String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@Length(min = 1, max = 250) String email) {
+        this.email = email;
+    }
+
+    public @Length(min = 8, max = 50) String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@Length(min = 8, max = 50) String password) {
+        this.password = password;
+    }
+}
