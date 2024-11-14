d607ae85d4ef69dfd1e8829488f9751426ff4976
Mateusz Kubiak <matkubiak888@gmail.com>
2024-11-14 03:34:10 +0100

    feat: Login DTO and Response

---
+++ services/user-service/src/main/java/com/github/matkubiak/taskplanner/model/LoginDTO.java
@@ -0,0 +1,31 @@
+/*
+ * Task Planner
+ * Copyright (C) Mateusz Kubiak
+ *
+ * Licensed under the GNU General Public License v3.
+ * See LICENSE or visit <https://www.gnu.org/licenses/>.
+ */
+
+package com.github.matkubiak.taskplanner.model;
+
+public class LoginDTO {
+
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ services/user-service/src/main/java/com/github/matkubiak/taskplanner/model/LoginResponse.java
@@ -0,0 +1,31 @@
+/*
+ * Task Planner
+ * Copyright (C) Mateusz Kubiak
+ *
+ * Licensed under the GNU General Public License v3.
+ * See LICENSE or visit <https://www.gnu.org/licenses/>.
+ */
+
+package com.github.matkubiak.taskplanner.model;
+
+public class LoginResponse {
+    private String token;
+
+    private long expiresIn;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public long getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(long expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+}
