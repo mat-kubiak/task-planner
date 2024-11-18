1f6a8c63d99a05366add89d5ea623cd9e54fcee8
Mateusz Kubiak <matkubiak888@gmail.com>
2024-11-19 00:00:59 +0100

    feat: auth services

--- services/user-service/src/main/java/com/github/matkubiak/taskplanner/repository/UserRepository.java
+++ services/user-service/src/main/java/com/github/matkubiak/taskplanner/repository/UserRepository.java
@@ -12,8 +12,12 @@ import com.github.matkubiak.taskplanner.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
---
+++ services/user-service/src/main/java/com/github/matkubiak/taskplanner/service/AuthenticationService.java
@@ -0,0 +1,62 @@
+/*
+ * Task Planner
+ * Copyright (C) Mateusz Kubiak
+ *
+ * Licensed under the GNU General Public License v3.
+ * See LICENSE or visit <https://www.gnu.org/licenses/>.
+ */
+
+package com.github.matkubiak.taskplanner.service;
+
+import com.github.matkubiak.taskplanner.model.*;
+import com.github.matkubiak.taskplanner.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthenticationService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public User signup(RegisterDTO input) throws SignupException {
+        if (userRepository.existsByEmail(input.getEmail())) {
+            throw new SignupException("This email is already registered!");
+        }
+
+        User user = new User();
+        user.setEmail(input.getEmail());
+        user.setPassword(passwordEncoder.encode(input.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+    public User authenticate(LoginDTO input) throws AuthenticationException, LoginException {
+        if (!userRepository.existsByEmail(input.getEmail())) {
+            throw new LoginException("This email is not registered!");
+        }
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(input.getEmail(), input.getPassword())
+        );
+
+        Optional<User> userOpt = userRepository.findByEmail(input.getEmail());
+        if (userOpt.isEmpty()) {
+            throw new LoginException("This email was registered before we authenticated it, but now it's gone. How did you manage to do this?");
+        }
+
+        return userOpt.get();
+    }
+}
