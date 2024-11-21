8c7ff8862331a575875e4b6f9d078890aefdcdb2
Mateusz Kubiak <matkubiak888@gmail.com>
2024-11-21 04:57:41 +0100

    build(deps): update jjwt to 0.12.6 (latest)

--- gateway/src/main/java/com/github/matkubiak/taskplanner/gateway/service/JwtService.java
+++ gateway/src/main/java/com/github/matkubiak/taskplanner/gateway/service/JwtService.java
@@ -8,22 +8,35 @@
 
 package com.github.matkubiak.taskplanner.gateway.service;
 
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
 @Service
 public class JwtService {
-    @Value("${security.jwt.secret-key}")
-    private String secretKey;
 
     @Value("${security.jwt.expiration-time}")
     private long jwtExpiration;
 
+    private final JwtParser jwtParser;
+
+    public JwtService(@Value("${security.jwt.secret-key}") String secretKey) {
+        byte[] decodedKey = Base64.getDecoder().decode(secretKey);
+        SecretKey key = new SecretKeySpec(decodedKey, 0, decodedKey.length, "HmacSHA256");
+
+        jwtParser = Jwts.parser()
+                .verifyWith(key)
+                .build();
+    }
+
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            jwtParser.parse(token);
             return true;
         } catch (Exception e) {
             return false;
