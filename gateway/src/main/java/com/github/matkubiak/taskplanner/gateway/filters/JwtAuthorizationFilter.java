c565906ccc4fa989454aea7622b52a3eb31046f5
Mateusz Kubiak <matkubiak888@gmail.com>
2024-11-21 03:59:02 +0100

    feat: implemented gateway filter

---
+++ gateway/src/main/java/com/github/matkubiak/taskplanner/gateway/filters/JwtAuthorizationFilter.java
@@ -0,0 +1,44 @@
+/*
+ * Task Planner
+ * Copyright (C) Mateusz Kubiak
+ *
+ * Licensed under the GNU General Public License v3.
+ * See LICENSE or visit <https://www.gnu.org/licenses/>.
+ */
+
+package com.github.matkubiak.taskplanner.gateway.filters;
+
+import com.github.matkubiak.taskplanner.gateway.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtAuthorizationFilter implements GatewayFilter {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+            return exchange.getResponse().setComplete();
+        }
+
+        String token = authHeader.substring(7);
+        if (!jwtService.validateToken(token)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+}
