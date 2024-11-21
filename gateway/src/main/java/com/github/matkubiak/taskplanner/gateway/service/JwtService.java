a993b8a3040e732c7060f562b2a8e927a37ed207
Mateusz Kubiak <matkubiak888@gmail.com>
2024-11-21 03:55:03 +0100

    feat: initial jwt service for verification

---
+++ gateway/src/main/java/com/github/matkubiak/taskplanner/gateway/service/JwtService.java
@@ -0,0 +1,32 @@
+/*
+ * Task Planner
+ * Copyright (C) Mateusz Kubiak
+ *
+ * Licensed under the GNU General Public License v3.
+ * See LICENSE or visit <https://www.gnu.org/licenses/>.
+ */
+
+package com.github.matkubiak.taskplanner.gateway.service;
+
+import io.jsonwebtoken.Jwts;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.expiration-time}")
+    private long jwtExpiration;
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
