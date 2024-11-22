3eb294b8f7f5bb260436fd3accd3bf43189e3718
Mateusz Kubiak <matkubiak888@gmail.com>
2024-11-22 16:41:51 +0100

    style: remove default var names & values

--- services/user-service/src/main/java/com/github/matkubiak/taskplanner/controller/AuthenticationController.java
+++ services/user-service/src/main/java/com/github/matkubiak/taskplanner/controller/AuthenticationController.java
@@ -17,11 +17,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class AuthenticationController {
 
     @Autowired
