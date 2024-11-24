7230b39673e7474512d073bb09be8c8f46123682
Mateusz Kubiak <matkubiak888@gmail.com>
2024-11-24 15:46:14 +0100

    feat: update Service methods to check user id

--- services/task-service/src/main/java/com/github/matkubiak/taskplanner/repository/TaskRepository.java
+++ services/task-service/src/main/java/com/github/matkubiak/taskplanner/repository/TaskRepository.java
@@ -12,7 +12,13 @@ import com.github.matkubiak.taskplanner.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findAllByUserId(Long userId);
+
+    boolean existsByIdAndUserId(Long id, Long userId);
 
 }
