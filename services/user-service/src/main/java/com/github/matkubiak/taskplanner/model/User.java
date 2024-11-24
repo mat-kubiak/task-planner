4e716ad74a73610cfdd6d4c08bae26ff62755420
Mateusz Kubiak <matkubiak888@gmail.com>
2024-11-24 17:24:19 +0100

    feat: added User model class

--- services/user-service/src/main/java/com/github/matkubiak/taskplanner/model/User.java
+++ services/user-service/src/main/java/com/github/matkubiak/taskplanner/model/User.java
@@ -9,8 +9,45 @@
 package com.github.matkubiak.taskplanner.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotNull
+    @Length(min=1, max=250)
+    private String email;
+
+    @NotNull
+    private String passwordHash;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public @Length(min = 1, max = 250) String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@Length(min = 1, max = 250) String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return passwordHash;
+    }
+
+    public void setPassword(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
 
 }
