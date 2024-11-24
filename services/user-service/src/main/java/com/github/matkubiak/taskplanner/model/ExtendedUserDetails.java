c39787057daa69110a343daf29131d291c5b5163
Mateusz Kubiak <matkubiak888@gmail.com>
2024-11-24 14:03:28 +0100

    feat: added ExtendedUserDetails interface with access to id

---
+++ services/user-service/src/main/java/com/github/matkubiak/taskplanner/model/ExtendedUserDetails.java
@@ -0,0 +1,16 @@
+/*
+ * Task Planner
+ * Copyright (C) Mateusz Kubiak
+ *
+ * Licensed under the GNU General Public License v3.
+ * See LICENSE or visit <https://www.gnu.org/licenses/>.
+ */
+
+package com.github.matkubiak.taskplanner.model;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface ExtendedUserDetails extends UserDetails {
+
+    Long getId();
+}
