d0eab6859caad6dcaef7c57d3135a5c3f54228d9
Mateusz Kubiak <matkubiak888@gmail.com>
2024-11-24 17:28:17 +0100

    feat: custom user details response class without sensitive data

---
+++ services/user-service/src/main/java/com/github/matkubiak/taskplanner/model/UserResponse.java
@@ -0,0 +1,30 @@
+/*
+ * Task Planner
+ * Copyright (C) 2024 Mateusz Kubiak
+ *
+ * Licensed under the GNU General Public License v3.
+ * See LICENSE or visit <https://www.gnu.org/licenses/>.
+ */
+
+package com.github.matkubiak.taskplanner.model;
+
+public class UserResponse {
+    private Long userId;
+    private String email;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
