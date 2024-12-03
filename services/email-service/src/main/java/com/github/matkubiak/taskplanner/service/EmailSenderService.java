dca2f90d19f127ed7c56484f9c59a1fe7ce9b2ac
Mateusz Kubiak <matkubiak888@gmail.com>
2024-12-03 15:39:23 +0100

    refactor: remove unused sendTextEmail method

--- services/email-service/src/main/java/com/github/matkubiak/taskplanner/service/EmailSenderService.java
+++ services/email-service/src/main/java/com/github/matkubiak/taskplanner/service/EmailSenderService.java
@@ -11,7 +11,6 @@ package com.github.matkubiak.taskplanner.service;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -21,16 +20,6 @@ public class EmailSenderService {
 
     @Autowired
     private JavaMailSender javaMailSender;
-
-    public void sendTextEmail(String address, String subject, String body) {
-        SimpleMailMessage msg = new SimpleMailMessage();
-
-        msg.setTo(address);
-        msg.setSubject(subject);
-        msg.setText(body);
-
-        javaMailSender.send(msg);
-    }
 
     public void sendHtmlEmail(String address, String subject, String body) throws MessagingException {
         MimeMessage msg = javaMailSender.createMimeMessage();
